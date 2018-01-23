6f2921a2d01de6a7a57271eb577a6367bfc440e7
vinmags16 <vinmags16@gmail.com>
2018-01-23 13:52:58 -0500

    Lab 1 Submission

--- system/main1.c
+++ system/main1.c
@@ -1,4 +1,4 @@
-/*  main.c  - main */
+/*  main1.c  - main */
 
 #include <xinu.h>
 #include <stdio.h>
@@ -16,6 +16,13 @@ process	main(void)
 	kprintf("\n...creating a shell\n");
 	recvclr();
 	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
+
+	/* Start onandon  */
+	int kidId = create((void*)onandon, 2048, 30, "onandon", 0, NULL);
+	resume(kidId);
+	sleep(14);
+	kill(kidId);
+	recvclr();
 
 	/* Wait for shell to exit and recreate it */	
 
