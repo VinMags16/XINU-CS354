2985ceeefbf24481d581fde077951ddd1da66557
vinmags16 <vinmags16@gmail.com>
2018-01-22 20:23:09 -0500

    Up to 3.4

--- system/main.c
+++ system/main.c
@@ -11,6 +11,8 @@ process	main(void)
 	kprintf("\nMy creator will turn itself into the do-nothing null process.\n");
 	kprintf("\nI will create a second XINU app that runs shell() in shell/shell.c as an example.\n");
 	kprintf("\nYou can do something else, or do nothing; it's completely up to you.\n");
+	kprintf("\nMaggioli,Vincent\n");
+	kprintf("\nvmaggiol\n");
 	kprintf("\n...creating a shell\n");
 	recvclr();
 	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
