62656d7f7a78d233a6cd50248aed10e8d1bba077
vinmags16 <vinmags16@gmail.com>
2018-01-29 16:52:00 -0500

    Changed main to match output and updated pdf

--- system/main.c
+++ system/main.c
@@ -11,7 +11,7 @@ process	main(void)
 	kprintf("\nMy creator will turn itself into the do-nothing null process.\n");
 	kprintf("\nI will create a second XINU app that runs shell() in shell/shell.c as an example.\n");
 	kprintf("\nYou can do something else, or do nothing; it's completely up to you.\n");
-	kprintf("\nMaggioli,Vincent\n");
+	kprintf("\n(Maggioli,Vincent)\n");
 	kprintf("\nvmaggiol\n");
 	kprintf("\n...creating a shell\n");
 	recvclr();
